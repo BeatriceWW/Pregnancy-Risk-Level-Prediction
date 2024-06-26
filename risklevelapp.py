9626810c46106cbd3447a57df69758fbe72ce405
BeatriceWW <bwwaihenya@gmail.com>
2024-06-26 21:12:03 +0300

    Update risklevelapp.py

--- risklevelapp.py
+++ risklevelapp.py
@@ -1,58 +1,47 @@
 import numpy as np 
 import streamlit as st 
 import pickle
-import os
 
 
 #Load saved model 
-#loaded_model = pickle.load(open('trained_model.sav', 'rb')) 
-
-model_path = os.path.join(os.path.dirname(__file__), 'trained_model.sav')
-try:
-    with open(model_path, 'rb') as model_file:
-        loaded_model = pickle.load(model_file)
-except Exception as e:
-    print(f"Error loading model: {e}")
+loaded_model = pickle.load(open('trained_model.sav', 'rb')) 
 
 #Create a prediction function 
-
 def risk_level_prediction(input_data):
-    try:
-        # Convert the input data into a numpy array
-        input_data_as_numpy = np.asarray(input_data, dtype=float)
+    
+    # Convert the input data into a numpy array
+    input_data_as_numpy = np.asarray(input_data)
 
-        # Reshape the array since we are predicting for one instance
-        input_data_reshape = input_data_as_numpy.reshape(1,-1)
+    # Reshape the array since we are predicting for one instance
+    input_data_reshape = input_data_as_numpy.reshape(1,-1)
 
-        # Making prediction
-        prediction = loaded_model.predict(input_data_reshape)
-        print(prediction)
+    # Making prediction
+    prediction = loaded_model.predict(input_data_reshape)
+    print(prediction)
 
-        if prediction[0] == 0:
-            return 'You have a high risk pregnancy, please seek urgent medical attention'
-        elif prediction[0] == 1:
-            return 'You have a low risk pregnancy'
-        elif prediction[0] == 2:
-            return 'You have medium risk pregnancy, please seek medical attention'
-        else:
-            return 'Error: unexpected prediction value'
-    except Exception as e:
-        return f"An error occurred: {str(e)}"
+    if prediction[0] == 0:
+        return 'High Risk: It is crucial to seek urgent medical attention to ensure the health and safety of both you and your baby. Please consult your healthcare provider immediately.'
+    elif prediction[0] == 1:
+        return 'Low Risk: Your pregnancy is currently considered low risk. Continue with your regular prenatal check-ups and maintain a healthy lifestyle.'
+    elif prediction[0] == 2:
+        return 'Medium Risk: It is important to seek medical attention to monitor and manage any potential complications. Please schedule an appointment with your healthcare provider soon'
+    else:
+        return 'Error: Please enter valid numerical values'
+ 
 
 def main():
     # Add title to web interface
-
     st.title('Pregnancy Risk Level Prediction')
 
     #get input from user 
     Age = st.text_input('Age')
-    Body_Temperature = st.text_input(' Body Temperature')
-    Heart_Rate = st.text_input(' Heart Rate')
-    Systolic_Blood_Pressure = st.text_input('  Systolic Blood Pressure')
-    Diastolic_Blood_Pressure = st.text_input('Diastolic_Blood_Pressure')
+    Body_Temperature = st.text_input('Body Temperature')
+    Heart_Rate = st.text_input('Heart Rate')
+    Systolic_Blood_Pressure = st.text_input('Systolic Blood Pressure')
+    Diastolic_Blood_Pressure = st.text_input('Diastolic  BloodPressure')
     BMI = st.text_input('BMI')
-    Blood_Glucose_HbA1c = st.text_input('Blood_Glucose_HbA1c')
-    Blood_Glucose_Fasting = st.text_input('Blood_Glucose_Fasting ')
+    Blood_Glucose_HbA1c = st.text_input('Blood Glucose HbA1c')
+    Blood_Glucose_Fasting = st.text_input('Blood Glucose Fasting')
 
     # Code for prediction 
 
@@ -60,12 +49,12 @@ def main():
 
     if st.button('Pregnancy Risk Level Result'):
         
-        diagnosis = risk_level_prediction([float(Age), float(Body_Temperature), float(Heart_Rate), 
-                                   float(Systolic_Blood_Pressure), float(Diastolic_Blood_Pressure),
-                                   float(BMI), float(Blood_Glucose_HbA1c), float(Blood_Glucose_Fasting)])
+        diagnosis = risk_level_prediction([Age, Body_Temperature, Heart_Rate, 
+                                   Systolic_Blood_Pressure, Diastolic_Blood_Pressure,
+                                   BMI, Blood_Glucose_HbA1c, Blood_Glucose_Fasting])
     
     st.success(diagnosis)
 
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main() 
